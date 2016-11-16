ffedfb4d1fda03d0cd1302cf2a31d388275b8598
zackattacknyu <zackattacknyu@gmail.com>
2016-11-15 16:38:37 -0800

    ok

--- hw6/problem2.m
+++ hw6/problem2.m
@@ -11,33 +11,17 @@ qMatrix = floor(rand(nn,nn).*10); %example trans probs
 initState = 4;
 numSteps = 100;
 
-times = zeros(1,numSteps); %time point
-states = zeros(1,numSteps); %current state at each point
-
-states(1) = initState;
-times(1) = 0;
-
-curT = 0;
-curState = initState;
-
-for i = 2:numSteps
-    
-    %lambdaBar to use to sample time to transition
-    lambdaBar = sum(qMatrix(curState,:));
-    
-    %sample time from exponential dist
-    sampledT = sampleFromDist(lambdaBar,1);
-    curT = curT + sampledT;
-    
-    %sample uniformly, then use lambda to decide which state
-    %   to transition to
-    sampledR = rand(1);
-    sampledLambda = sampledR*lambdaBar;
-    curState = find(cumsum(qMatrix(curState,:))>sampledLambda,1,'first');
-    
-    states(i) = curState;
-    times(i) = curT;
-    
+numPlots = 3;
+timesArr = cell(1,numPlots);
+statesArr = cell(1,numPlots);
+for jj = 1:numPlots
+    [timesArr{jj},statesArr{jj}] = computeTimecourse(qMatrix,initState,numSteps);
 end
 
-plot(times,states)
+figure
+hold on
+for kk = 1:numPlots
+   plot(timesArr{kk},statesArr{kk}); 
+end
+hold off
+
